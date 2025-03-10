3ae23181fbb12eda35d1762146a9e3c5d2a3f15c
左天亦 <tianyi.zuo@selfweldrobot.com>
2025-03-10 08:46:51 +0000

    new docker with drake, ceres and vtune profiler

--- src/ResourceManager.h
+++ src/ResourceManager.h
@@ -3,28 +3,28 @@
 #include "Resource.h"
 #include <QObject>
 
-
 class ResourceManager : public QObject {
-    Q_OBJECT
+  Q_OBJECT
 
 public:
-    explicit ResourceManager(QObject *parent = nullptr);
-    std::vector<Resource *> getRootResources() const;
+  explicit ResourceManager(QObject *parent = nullptr);
+  std::vector<Resource *> getRootResources() const;
 
-public slots: 
-    void addResource(Resource *parent, const std::string &name, const ResourceType type);
-    void deleteResource(Resource *resource);
-    void renameResource(Resource *resource, const std::string &newName);
-    void sortResources(const std::string &criteria, const sortOrder &order);
-    void insertChild(Resource *parent, Resource *child, std::size_t index);
-    void removeChild(Resource *child);
-    void createTestData();
+public slots:
+  void addResource(Resource *parent, const std::string &name,
+                   const ResourceType type);
+  void deleteResource(Resource *resource);
+  void renameResource(Resource *resource, const std::string &newName);
+  void sortResources(const std::string &criteria, const sortOrder &order);
+  void insertChild(Resource *parent, Resource *child, std::size_t index);
+  void removeChild(Resource *child);
+  void createTestData();
 
 signals:
-    void resourceUpdated(); 
+  void resourceUpdated();
 
 private:
-    std::vector<Resource *> rootResources;
+  std::vector<Resource *> rootResources;
 };
 
 #endif // RESOURCEMANAGER_H
